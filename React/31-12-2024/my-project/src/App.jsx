b41340c67d29b20f6816444d19ab182dcf934d67
Sohail Shoukat <sohailshoukat131@gmail.com>
2024-12-31 19:05:48 +0500

    initial commit

---
+++ React/31-12-2024/my-project/src/App.jsx
@@ -0,0 +1,27 @@
+// import React from 'react'
+
+import React from "react";
+import { useState } from "react";
+
+function App() {
+  const [data, setData] = useState("");
+  const [text, setText] = useState("");
+
+  function change(e) {
+    console.log(e.target.value);
+    setData(e.target.value);
+  }
+  function changeValue(event) {
+    setText(data);
+  }
+  return (
+    <div className="App">
+      <p>Hello {text} </p> <br />
+      <input onChange={change} />
+      <br />
+      <button onClick={changeValue}>Click</button>
+    </div>
+  );
+}
+
+export default App;
