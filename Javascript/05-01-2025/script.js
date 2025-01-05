315131a50bce0a66928b517cc867f1774d1f9436
Sohail Shoukat <sohailshoukat131@gmail.com>
2025-01-05 11:56:56 +0500

    alerts

---
+++ Javascript/05-01-2025/script.js
@@ -0,0 +1,30 @@
+console.log("Today We are going to learn JavaScript👨‍🏫 !");
+console.log("I hope you will be exicited to learn conding 👨‍💻");
+
+//Basic Alerts
+alert("Welcome to Javascript Code !");
+//Confirmation Alerts
+var result = confirm("Do you want to delete?");
+if (result) {
+  alert("Your message has been successfully deleted");
+} else {
+  alert("Your message has not been deleted.");
+}
+
+// Prompt Alerts
+var name = prompt("What is your name ?");
+console.log("Your name is " + name);
+// Practice Task
+alert("Hello World I Love JavaScript !");
+alert("Thanks for your input !");
+alert(" JavaScript ignores spaces ");
+
+//Some coders write
+window.alert("Some coders wirte window.alert");
+
+// Note
+// Alert is a box  that  pop up to give a message to user.
+// Alert is a keyword.
+// alert isn't capitalized Alert("Hello ")
+// JavaScript ignores spaces
+//  In JavaScript, alert() is a built-in function
