d4f39971437edc538842292e270518fe8c90e733
Sohail Shoukat <sohailshoukat131@gmail.com>
2025-01-05 12:34:45 +0500

    👉chapter 01

---
+++ Javascript/Javascript Exercises/Chapter 01/index.js
@@ -0,0 +1,9 @@
+// Assignment #01
+alert("Hello 👋 How are you ?");
+alert("Error! Please enter a valid a valid password.");
+alert("Welcome to Js Land.. \nHappy Coding!");
+alert("Welcome to Js Land");
+alert("Happy Coding !");
+alert("Hello.. I can run JS through my web browsers console");
+document.write("Welcome to Js Land.. <br> Happy Coding!");
+document.write(" <br>Thanks❤✨");
