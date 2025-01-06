3e7ff7c4d8f38d8d3a259affd0ca01e242e3b130
Sohail Shoukat <sohailshoukat131@gmail.com>
2025-01-06 11:56:30 +0500

    Variables Names Legal &  ILLegal

---
+++ Javascript/6-01-2025/Variables Names Legal & ILegal/index.js
@@ -0,0 +1,36 @@
+//JavaScript Mein Variable Names: Legal Aur Illegal
+// Letters (A to Z, a to z): Variable name mein alphabet ka use kar sakte hain
+// Example : name , age , city
+
+// Numbers (0-9) but not at the beginning:
+// Variable name mein numbers ka istemal kar sakte hain, lekin wo name ke pehle nahi aayein.
+// Example : user1, score3 Sahi hai
+
+// Underscore (_) aur Dollar Sign ($):
+//  Aap _ ya $ ko variable name mein shamil kar sakte hain.
+// Example : _temp , $price
+
+// camelCase Words use kr saktein hai
+// Example : userName , totalAmount;
+
+// Keywords:
+// Aap JavaScript ke reserved words ko variable name nahi bana sakte.
+// Jaise break, class, for, if, return etc.
+
+// ILLegal Names
+// var break  = "Hello" ===> Galat hai
+// Starting with a Number:
+//  Agar aap variable name ko number se shuru karte hain, toh wo galat hoga.
+// Example : 2name, 4price
+// Whitespace: Variable name mein space nahi ho sakti.
+//  Agar space dena ho toh underscore (_) ya camelCase ka use karein.
+// Example : price  Amount
+
+// Task to perform
+
+var name = "Sohail Shoukat";
+// name is a variable
+var age = 20;
+// age is a variable
+var totalPrice = 150;
+// totalPrice is a varable
