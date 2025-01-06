2717605fb4d532962f4fc691c30cf5dea71bff07
Sohail Shoukat <sohailshoukat131@gmail.com>
2025-01-06 12:15:09 +0500

    👉chapter 04

---
+++ Javascript/Javascript Exercises/Chapter 04/index.js
@@ -0,0 +1,19 @@
+// Assignment #04
+// 01
+var myName;
+var toralPrice;
+var user_name;
+// 02
+// Declare 5 Legal Variables
+var name = "Js";
+console.log(name);
+var $price = 23;
+console.log($price);
+var num1 = 10;
+var num2 = 10;
+var result = 10 + 10;
+console.log(result);
+var my_age = 19;
+console.log(my_age);
+var totalScore = 120;
+console.log(totalScore);
