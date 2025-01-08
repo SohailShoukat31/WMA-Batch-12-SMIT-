0bac7416f31fa7ffd291cd333579c6ef6e86ea2a
Sohail Shoukat <sohailshoukat131@gmail.com>
2025-01-08 18:41:31 +0500

    👉oneShot Js🙂

---
+++ Javascript (OneShot)/01_Variables/02_DataTypes/index.js
@@ -0,0 +1,43 @@
+// let name = "Sohail"
+// let age = 18;
+// let isLoggedIn = false;
+
+// Primitive Data Types
+// String ==> "" | ''
+let name = "Sohail";
+// Number
+let age = 19;
+let price = 99.99;
+// Boolean
+let isStudent = true;
+let isLoggedIn = false;
+// Undefined
+let myVar;
+console.log(typeof myVar);
+// Null
+let emptyValue = null;
+console.log(typeof emptyValue); // null is return object
+// Symbol (ES6)  // Unique identifier
+//BigInt
+// Bohat bade numbers store karne ke liye.
+let BigInt = 1234567890n;
+console.log(BigInt);
+//  Non-Primitive Data Types
+// Object
+let person = {
+  name: "Sohail",
+  age: 19,
+};
+console.log(person);
+// Array
+let fruits = ["Apple", "Banana", "Mango"];
+console.log(fruits);
+
+let a = 10;
+let b = a; // copy
+b = 20;
+console.log(b);
+
+// Note
+
+//JavaScript mein data ko alag-alag tareeqon se store kiya ja sakta hai. In tareeqon ko "Data Types" kehte hain
---
+++ Javascript (OneShot)/01_Variables/index.js
@@ -0,0 +1,20 @@
+console.log("Hello World !");
+
+const accountId = "12345";
+let accountEmail  = "sohail@gmail.com";
+var accountPassword = "000786";
+var accountCity;
+
+
+// accountId = "1234"
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword , accountCity ]);
+
+// var (ES5 se pehle kaam aata tha)
+// let (Modern aur block-scope wala)
+// const (Fixed value ke liye)
+
+
+
+
+ 
