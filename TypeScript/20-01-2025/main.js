2612b41b86a15ad1df466f9b65c5c8ac721d585c
Sohail Shoukat <sohailshoukat131@gmail.com>
2025-01-20 12:19:32 +0500

    Ts Simple Types

---
+++ TypeScript/20-01-2025/main.js
@@ -0,0 +1,15 @@
+console.log("Hello World ");
+// TS Simple Types
+var isStudent = true;
+console.log(isStudent);
+var myName = "sohail";
+console.log(myName);
+var number = 123;
+console.log(number);
+// number = ""
+// Explicit
+var firstName = "Sohail";
+console.log(firstName);
+// Implicit
+var lastName = "Sohail";
+console.log(lastName);
---
+++ TypeScript/20-01-2025/main.ts
@@ -0,0 +1,25 @@
+console.log("Hello World ");
+
+// TS Simple Types
+const isStudent: boolean = true;
+console.log(isStudent);
+
+let myName: string = "sohail";
+console.log(myName);
+
+let number: number = 123;
+console.log(number);
+
+// number = ""
+
+// Explicit
+let firstName: string = "Sohail";
+console.log(firstName);
+// Implicit
+let lastName = "Shoukat";
+console.log(lastName);
+
+// Error In Type Assignment
+
+// let myAge:number = 19;
+// myAge = "Nineteen"
