423c6bc77f06bedcadf274c90decc8081f941f0e
Sohail Shoukat <sohailshoukat131@gmail.com>
2025-01-28 10:46:49 +0500

    28-01-25

---
+++ Javascript/28-01-2025/index.js
@@ -0,0 +1,11 @@
+console.log("Hello World ");
+let a = "xyz";
+console.log(a);
+let b = 123;
+console.log(b);
+let c = true;
+console.log(c);
+let myName = "sohail";
+let age = 19;
+let isStudent = true;
+console.log(`My name is ${myName} and my age is : ${age} `);
