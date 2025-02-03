4832a157bda85701af720cc70bbf027d41547149
Sohail Shoukat <sohailshoukat131@gmail.com>
2025-02-03 09:26:11 +0500

    Data Types

---
+++ Javascript/03-02-2025/index.js
@@ -0,0 +1,48 @@
+console.log("Hello World !");
+
+let message = "hello";
+console.log(message);
+message = 12345;
+// No error
+console.log(message);
+
+let n = 123;
+n = 34.45;
+console.log(n);
+
+// Infinity
+
+console.log(1 / 0);
+console.log(Infinity);
+
+let num1 = "Thirty Three";
+let num2 = 33;
+console.log(num1 - num2);
+
+console.log(NaN + 1);
+console.log(NaN - 1);
+console.log(NaN ** 1);
+
+let name = "John";
+console.log(`Hello ${name} How are you ?`);
+
+let number1 = 1 + 5;
+console.log(`the result is 1 + 5 is ${number1}`);
+
+let isStudent = true;
+let isTeacher = false;
+
+let isGreater = 4 > 2;
+console.log(isGreater);
+let isLess = 4 < 2;
+console.log(isLess);
+
+let age = null;
+console.log(age);
+
+let myAge;
+console.log(myAge);
+
+let value = 100;
+value = undefined;
+console.log(value);
