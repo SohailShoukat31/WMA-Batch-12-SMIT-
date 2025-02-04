4bdc49c82542e9b3aaee3c5fb778f09b17a3037f
Sohail Shoukat <sohailshoukat131@gmail.com>
2025-02-04 08:03:36 +0500

    .

---
+++ Javascript/04-02-2025/index.js
@@ -0,0 +1,47 @@
+console.log("Hello JavaScript ❤");
+console.log("Hello Second Line");
+// Code Structure
+console.log("Hello World ");
+console.log("Hello World ");
+
+console.log("Hello World");
+console.log("Hello World ");
+
+console.log(3 + 1 + 2);
+console.log(3 + 1 + 2);
+
+// Variables
+
+let message;
+console.log(message); // Undefined
+message = "Hello";
+console.log(message); // Hello
+
+let user = "Sohail";
+age = 19;
+roll = "Web Developer";
+console.log(
+  ` Username is :   ${user} \n User age is :  ${age} \n User roll is : ${roll} `
+);
+
+let hello = "hello world !";
+console.log(hello);
+let anotherHello = hello;
+console.log(anotherHello);
+
+let name;
+name = "John";
+let admin;
+admin = name;
+alert(admin);
+
+let ourPlanetName = "Earth";
+console.log(ourPlanetName);
+
+let currentUserName = "Sohail";
+console.log(currentUserName);
+
+// const birthday = '18.04.1982';
+
+// const age = someCode(birthday);
+// console.log(age);
