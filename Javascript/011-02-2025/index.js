addd4623ad6ec0339c8d07e49a9c3078c96a5189
Sohail Shoukat <sohailshoukat131@gmail.com>
2025-02-11 11:03:16 +0500

    function

---
+++ Javascript/011-02-2025/index.js
@@ -0,0 +1,19 @@
+console.log("Hello world!");
+
+function abc() {
+  // Code
+}
+
+// Call
+
+function abc() {
+  let a = "Sohail";
+  console.log(a);
+}
+abc();
+abc();
+
+function show() {
+  alert("Hello I Love JavaScript ❤✨");
+  alert("Thank You ");
+}
